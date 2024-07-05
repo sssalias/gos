96dff77dd8f55796594d1249016708abea8a14ce
sssalias <top.chel.05@mail.ru>
2024-07-05 16:07:56 +0300

    fixed audio

--- src/api/providers/notifications-provider/index.tsx
+++ src/api/providers/notifications-provider/index.tsx
@@ -3,20 +3,25 @@ import { Bounce, toast, ToastContainer } from 'react-toastify'
 import { onMessage } from 'firebase/messaging'
 import { messaging, requestPermission } from 'src/firabse'
 import { useUserStore } from 'src/store/user'
+import useSound from 'use-sound'
+
+import sound from 'src/assets/sound.mp3'
+
 type PropsType = {
     children: ReactNode
 }
 
 const NotificationsProvider = ({children}:PropsType) => {
 
-    const audio = new Audio('src/assets/sound.mp3')
     const {token} = useUserStore()
+
+    const [play] = useSound(sound, {volume: 0.1})
 
     useEffect(() => {
         if (token.length !== 0) {
             requestPermission(token)
             onMessage(messaging, (payload) => {
-                audio.play()
+                play()
                 toast.info(<div>
                     <h2>{payload.notification?.title}</h2>
                     <p>{payload.notification?.body}</p>
