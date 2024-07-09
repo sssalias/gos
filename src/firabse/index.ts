92a0ac90efec8424a77dcb4192445c050b88f155
sssalias <top.chel.05@mail.ru>
2024-07-09 11:34:21 +0300

    asfsaasf

--- src/firabse/index.ts
+++ src/firabse/index.ts
@@ -19,6 +19,7 @@ export const messaging = getMessaging(app)
 
 export async function requestPermission(accesToken: string) {
   const permission = await Notification.requestPermission()
+  //@ts-ignore
   const swRegistration = await navigator.serviceWorker.register('/firebase-messaging-sw.js')
   if (permission === 'granted') {
     const token = await getToken(messaging, {vapidKey: 'BKwVMxZ5bB9H7o5hjGK2iK5T6aGwnEgb2O1dcHz3GxiB7tTRfOfVmD1vJqg4m_b3OFkmadzwPXqCrtyfArqa7jQ'})
