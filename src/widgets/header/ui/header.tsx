81c349d18767ee316b13ec9ed65270bc1fe6cd3c
Alexey <top.chel.05@mail.ru>
2024-09-15 19:51:45 +0300

    created: header

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,10 +1,37 @@
-import { Navbar } from '@nextui-org/react'
+import { User } from '@nextui-org/react'
+import { useKeycloak } from '@react-keycloak/web'
+import { useEffect, useState } from 'react'
+import { jwtDecode } from 'jwt-decode'
+
 
 const Header: React.FC = () => {
-    return (
-        <Navbar className='bg-red-300'>
 
-        </Navbar>
+    const {keycloak, initialized} = useKeycloak()
+    const [data, setData] = useState({
+        name: '',
+        role: ''
+    })
+
+    useEffect(() => {
+        if (keycloak.token && keycloak.token) {            
+            //@ts-ignore
+            if (keycloak.resourceAccess['kozodoy-client'].roles.includes('admin')) {
+                setData({...data, role: 'Администатор'})
+            //@ts-ignore
+            } else if (keycloak.resourceAccess['kozodoy-client'].roles.includes('manager')) {
+                setData({...data, role: 'Менеджер'})
+            }
+        }
+    }, [initialized, keycloak])
+
+    return (
+        <header className='w-full flex'>
+            <User
+                name={data.name}
+                description={data.role}
+                className='ml-auto'
+            />
+        </header>
     )
 }
 export default Header
