df243022da2f4de35160fa4b26628b448635bb24
Alexey <top.chel.05@mail.ru>
2024-09-15 10:40:22 +0300

    created: v.2 branch

--- src/auth/kk-config/index.ts
+++
@@ -1,8 +0,0 @@
-import Keycloak from 'keycloak-js'
-
-
-export const authClient = new Keycloak({
-    url: `${import.meta.env.VITE_KK_HOST}`,
-    realm: `${import.meta.env.VITE_REALM}`,
-    clientId: `${import.meta.env.VITE_CLIENT_ID}`,
-})
