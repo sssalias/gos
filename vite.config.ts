e3831f25c6e969f356f16d08adee4d69edffa5c5
Alexey <top.chel.05@mail.ru>
2024-09-26 10:57:26 +0300

    fixed: checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import checker from 'vite-plugin-checker'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), checker({typescript: false})],
+  plugins: [react()],
   resolve: {
     alias: {
       src: '/src',
